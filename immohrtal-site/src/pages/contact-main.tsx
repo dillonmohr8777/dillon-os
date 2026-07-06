06a93445340fe9982225ddcc2668c472e011cfa1
dillonmohr8777 <dillonmohr8777@gmail.com>
2026-07-05 21:19:54 -0400

    Load growth styles on contact entry

--- immohrtal-site/src/pages/contact-main.tsx
+++ immohrtal-site/src/pages/contact-main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
+import '../immohrtal-growth.css'
+import '../immohrtal-design-upgrade.css'
 import { ContactPage } from './ContactPage'
 
 createRoot(document.getElementById('root')!).render(
