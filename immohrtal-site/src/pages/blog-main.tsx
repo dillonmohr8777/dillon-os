f86e0f0ce07a0b880041df4a8d21b7de83a32a8c
dillonmohr8777 <dillonmohr8777@gmail.com>
2026-07-05 21:19:52 -0400

    Load growth styles on blog entry

--- immohrtal-site/src/pages/blog-main.tsx
+++ immohrtal-site/src/pages/blog-main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
+import '../immohrtal-growth.css'
+import '../immohrtal-design-upgrade.css'
 import { BlogPage } from './BlogPage'
 
 createRoot(document.getElementById('root')!).render(
