d8ec1cf64e81375143e6cc0c8a55ab1581da9703
dillonmohr8777 <dillonmohr8777@gmail.com>
2026-07-05 21:19:53 -0400

    Load growth styles on about entry

--- immohrtal-site/src/pages/about-main.tsx
+++ immohrtal-site/src/pages/about-main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
+import '../immohrtal-growth.css'
+import '../immohrtal-design-upgrade.css'
 import { AboutPage } from './AboutPage'
 
 createRoot(document.getElementById('root')!).render(
